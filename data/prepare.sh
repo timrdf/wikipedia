168ebf0cc85f9e6d7c75aadb234b82dfd690e1df
Tim L <timrdf@gmail.com>
2016-03-14 16:34:45 -0400

    variable-ized automatic dir

--- data/prepare.sh
+++ data/prepare.sh
@@ -5,7 +5,12 @@
 #3>    rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers>;
 #3> .
 
-mkdir -p automatic
+automatic='automatic'
+mkdir -p $automatic
 for page in `find quoted -name "*.xml"`; do
-   ../bin/saxon.sh ../src/mw2prov.xsl xml ttl -w -od automatic $page
+   if [[ ! -e $automatic/`basename $page.ttl` ]]; then
+      ../bin/saxon.sh ../src/mw2prov.xsl xml ttl -w -od $automatic $page
+   else
+      echo "$automatic/$page.ttl already exists."
+   fi
 done
